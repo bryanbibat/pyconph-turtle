6bee631d1a3785ad2bdb8e2eb7a41063ac74f494
Bry <bry@bryanbibat.net>
2012-07-01 15:21:40 +0800

    create .gitignore and dummy files

---
+++ turtle2_bak.py
@@ -0,0 +1,34 @@
+import turtle
+
+def draw_f():
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(180)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(180)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(200)
+    turtle.left(90)
+
+def draw_e():
+    draw_f()
+    turtle.forward(100)
+
+turtle.penup()
+turtle.right(180)
+turtle.forward(125)
+turtle.pendown()
+turtle.right(225)
+draw_e()
+turtle.penup()
+turtle.forward(50)
+turtle.pendown()
+draw_f()
+turtle.exitonclick()
